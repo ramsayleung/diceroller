377d57e36515b089e0f0c8e1a6db35aa20965620
Ramsay Leung <ramsayleung@gmail.com>
2024-04-05 16:55:53 -0700

    Add DiceSetting model and view.

---
+++ DiceRoller/Model/Dice.swift
@@ -0,0 +1,14 @@
+//
+//  Dice.swift
+//  DiceRoller
+//
+//  Created by ramsayleung on 2024-04-05.
+//
+
+import Foundation
+
+@Observable
+class DiceSetting {
+    var numberOfDices = 5
+    var numberOfSlices = 6
+}
---
+++ DiceRoller/SettingView.swift
@@ -0,0 +1,54 @@
+//
+//  SettingView.swift
+//  DiceRoller
+//
+//  Created by ramsayleung on 2024-04-05.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    @Binding var numberOfDices: Int
+    @Binding var numberOfSlices: Int
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section("Number of Dice") {
+                    Text("Dices: \(numberOfDices.formatted(.number))")
+                        .font(.headline)
+                        .padding()
+                    
+                    CustomSlider(sliderValue: numberOfDicesProxy, inRange: 1...5, step: 1)
+                }
+                
+                Section("Number of Slices") {
+                    Text("Slices: \(numberOfSlices)")
+                        .font(.headline)
+                        .padding()
+                    Picker("Please choose a color", selection: $numberOfSlices) {
+                        ForEach([4,6,10,15,20,100], id: \.self) {
+                            Text("\($0)")
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            }
+        }
+    }
+    
+    var numberOfDicesProxy: Binding<Double> {
+        Binding<Double>(
+            get: {
+                return Double(numberOfDices)
+            },
+            set: {
+                numberOfDices = Int($0)
+            }
+        )
+    }
+}
+
+#Preview {
+    @State  var numberOfSlices = 6
+    @State  var numberOfDices = 5
+    return SettingView(numberOfDices: $numberOfDices, numberOfSlices: $numberOfSlices)
+}
