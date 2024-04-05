97bc54e71aae04031a8cb3f4a5d40718da793492
Ramsay Leung <ramsayleung@gmail.com>
2024-04-04 22:07:05 -0700

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by ramsayleung on 2024-04-04.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
